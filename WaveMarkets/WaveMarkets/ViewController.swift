eaeb3561aacf209c2cd9b3b939e65593deeaa81f
Adrian <34623084+JavaCup4U@users.noreply.github.com>
2025-04-18 16:12:45 -0500

    initial commit

---
+++ WaveMarkets/WaveMarkets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaveMarkets
+//
+//  Created by Adrian Reyes on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
