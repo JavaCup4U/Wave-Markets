c4bee60747b6dca8f705424c3ca94767890a05d8
Adrian <34623084+JavaCup4U@users.noreply.github.com>
2025-04-22 15:19:24 -0500

    stream screen updates
    -Updated Stream Screen

---
+++ WaveMarkets/WaveMarkets/StockCell.swift
@@ -0,0 +1,36 @@
+//
+//  StockCell.swift
+//  WaveMarkets
+//
+//  Created by Adrian Reyes on 4/22/25.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+    
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+    
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WaveMarkets/WaveMarkets/ViewController.swift
+++ WaveMarkets/WaveMarkets/ViewController.swift
@@ -7,9 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return number of rows for table
+        return stockList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //create , configure , and return a table view cell for the given row
+        // create cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StockCell", for: indexPath) as! StockCell
+        let stock = stockList[indexPath.row]
+        // configure cell
+        cell.symbolLabel?.text = stock.symbol.first ?? "N/A"
+        // get the row where the cell will be placed using row property
+        cell.priceLabel?.text  = "$\(String(format: "%.2f", stock.price))"
+        // return the cell for use in respective table view row
+        return cell
+    }
+    
+    
+    @IBOutlet weak var streamLabel: UILabel!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
